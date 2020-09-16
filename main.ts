c86e5584fa3803b999c3928a6782aa6c4e6531b8
Beedlebub <beedlebub@gmail.com>
2020-09-16 16:41:31 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.Target)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+})
+basic.showIcon(IconNames.Target)
